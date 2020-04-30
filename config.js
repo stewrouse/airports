5782e1b812a5b2ff22be3dd153bc5cb5838b950d
Stewart Rouse <srouse@esri.com>
2020-04-30 12:29:36 -0400

    Update config.js

--- config.js
+++ config.js
@@ -1,49 +1,269 @@
-params =
+params=
+
 {
-    "aircraftList": ["A 300", "A 320"],
 
-    "renderer": {
-        "type": "unique-value",
-        "defaultSymbol": {
-            "type": "simple-fill"
-        },
-        "field": "Aircraft_Type",
-        "uniqueValueInfos": [
-            {
-                "value": "A 300",
-                "symbol": {
-                    "type": "simple-fill",
-                    "color": [
-                        156,
-                        209,
-                        132
-                    ]
-                }
-            },
-            {
-                "value": "A 320",
-                "symbol": {
-                    "type": "simple-fill",
-                    "color": [
-                        200,
-                        135,
-                        49
-                    ]
-                }
-            }
-        ]
+"aircraftList":["A 300","A 319","A 320","A 321","A330_200","A330_300","A340_600","A350_900","B 737_800","B737_900","B747_400","B757_200","B757_300","B767_200","B767_300W","B777_200","B777_300ER","B787_800","B787_900","C_17","Beechcraft_1900C"],
+
+"renderer":{
+    "type": "unique-value",
+    "defaultSymbol": {
+        "type": "simple-fill"
     },
-
-    "aircraftLayerURL": "https://services.arcgis.com/6I1ysurtNWNxkuwd/ArcGIS/rest/services/PHX_Aircraft_AZ_State_Plane/FeatureServer/0",
-
-    "fieldConfig": [
+    "field": "Aircraft_Type",
+    "uniqueValueInfos": [
         {
-            "name": "Carrier",
-            "label": "Carrier"
+            "value": "A 300",
+            "symbol": {
+                "type": "simple-fill",
+                "color": [
+                    159,
+                    224,
+                    25
+                ]
+            }
         },
         {
-            "name": "Date_Out",
-            "label": "Date Out"
+            "value": "A 319",
+            "symbol": {
+                "type": "simple-fill",
+                "color": [
+                    175,
+                    248,
+                    47
+                ]
+            }
+        },
+        {
+            "value": "A 320",
+            "symbol": {
+                "type": "simple-fill",
+                "color": [
+                    135,
+                    175,
+                    88
+                ]
+            }
+        },
+        {
+            "value": "A 321",
+            "symbol": {
+                "type": "simple-fill",
+                "color": [
+                    231,
+                    168,
+                    117
+                ]
+            }
+        },
+        {
+            "value": "A330_200",
+            "symbol": {
+                "type": "simple-fill",
+                "color": [
+                    220,
+                    171,
+                    16
+                ]
+            }
+        },
+        {
+            "value": "A330_300",
+            "symbol": {
+                "type": "simple-fill",
+                "color": [
+                    8,
+                    61,
+                    169
+                ]
+            }
+        },
+        {
+            "value": "A340_600",
+            "symbol": {
+                "type": "simple-fill",
+                "color": [
+                    160,
+                    30,
+                    215
+                ]
+            }
+        },
+        {
+            "value": "A350_900",
+            "symbol": {
+                "type": "simple-fill",
+                "color": [
+                    33,
+                    201,
+                    96
+                ]
+            }
+        },
+        {
+            "value": "B 737_800",
+            "symbol": {
+                "type": "simple-fill",
+                "color": [
+                    130,
+                    58,
+                    116
+                ]
+            }
+        },
+        {
+            "value": "B737_900",
+            "symbol": {
+                "type": "simple-fill",
+                "color": [
+                    195,
+                    221,
+                    98
+                ]
+            }
+        },
+        {
+            "value": "B747_400",
+            "symbol": {
+                "type": "simple-fill",
+                "color": [
+                    33,
+                    76,
+                    80
+                ]
+            }
+        },
+        {
+            "value": "B757_200",
+            "symbol": {
+                "type": "simple-fill",
+                "color": [
+                    156,
+                    14,
+                    144
+                ]
+            }
+        },
+        {
+            "value": "B757_300",
+            "symbol": {
+                "type": "simple-fill",
+                "color": [
+                    60,
+                    165,
+                    232
+                ]
+            }
+        },
+        {
+            "value": "B767_200",
+            "symbol": {
+                "type": "simple-fill",
+                "color": [
+                    32,
+                    245,
+                    242
+                ]
+            }
+        },
+        {
+            "value": "B767_300W",
+            "symbol": {
+                "type": "simple-fill",
+                "color": [
+                    152,
+                    182,
+                    187
+                ]
+            }
+        },
+        {
+            "value": "B777_200",
+            "symbol": {
+                "type": "simple-fill",
+                "color": [
+                    244,
+                    199,
+                    53
+                ]
+            }
+        },
+        {
+            "value": "B777_300ER",
+            "symbol": {
+                "type": "simple-fill",
+                "color": [
+                    101,
+                    198,
+                    45
+                ]
+            }
+        },
+        {
+            "value": "B787_800",
+            "symbol": {
+                "type": "simple-fill",
+                "color": [
+                    233,
+                    7,
+                    110
+                ]
+            }
+        },
+        {
+            "value": "B787_900",
+            "symbol": {
+                "type": "simple-fill",
+                "color": [
+                    14,
+                    75,
+                    26
+                ]
+            }
+        },
+        {
+            "value": "C_17",
+            "symbol": {
+                "type": "simple-fill",
+                "color": [
+                    237,
+                    75,
+                    130
+                ]
+            }
+        },
+        {
+            "value": "Beechcraft_1900C",
+            "symbol": {
+                "type": "simple-fill",
+                "color": [
+                    21,
+                    203,
+                    217
+                ]
+            }
         }
     ]
+},
+
+"aircraftLayerURL":"https://services.arcgis.com/6I1ysurtNWNxkuwd/ArcGIS/rest/services/PHX_Aircraft_AZ_State_Plane/FeatureServer/0",
+
+"fieldConfig":[
+    {
+        "name": "Tail_Number",
+        "label": "Tail_Number"
+    },
+    {
+        "name": "Carrier",
+        "label": "Carrier"
+    },
+    {
+        "name": "Date_In",
+        "label": "Date In"
+    },
+    {
+        "name": "Date_Out",
+        "label": "Date Out"
+    }
+]
+
 }
